a120e88313f5a531676db710b4f30f1be4330d7a
HappyMan <100917359+NPChappyman@users.noreply.github.com>
2025-01-02 16:48:50 +0300

    17.11

--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,29 @@
 import LinkedList.SortedList;
 import doublyLinked.DoubleLink;
 import doublyLinked.DoubleLinkedListIterator;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import Tree.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        /*int[] arr = {1,2,3,4,5};
-        System.out.println(Array.array.sortfind(arr,4));
-
-        long[] arr2 = {3,6,2,1,8,7};
-        long[] workplace = new long[6];
-        Array.sort.recMergeSort(arr2,workplace,0,5);
-        for (int i =0 ; i<6;i++) System.out.println(arr2[i]);*/
-        doublyLinked.DoubleLink wwww = new doublyLinked.DoubleLink();
-        doublyLinked.DoubleLinkedListIterator it2 = new DoubleLinkedListIterator(wwww);
-        it2.insertAfter(10);
-        it2.insertAfter(291);
-        it2.insertAfter(7);
-
-        wwww.deleteKey(291);
-        wwww.deleteKey(7);
-
-        wwww.deleteKey(10);
-        wwww.displayForward();
-        System.out.println(power(2,62));
-        int[] arr = new int[] {7,1,5,9,2};
-        int[] arr1 = new int[arr.length];
-        bag(arr,arr1,0,0,14);
-        int[] warr = new int[] {90,100,20,60,80,110,120,40,10,30,50,70};
-        Array.sort.recQuickSort(warr,0, warr.length-1);
-        Array.sort.display(warr, warr.length);
-
-        String[] climbers = {"A", "B", "C", "D", "E"};
-
-        // Размер команды (N)
-        int teamSize = 3;
-
-        // Массив для хранения текущей комбинации
-        String[] currentCombination = new String[teamSize];
-
-        // Генерация всех сочетаний размера teamSize
-        System.out.println("Все возможные команды из " + teamSize + " альпинистов:");
-        generateCombinations(climbers, teamSize, 0, currentCombination, 0);
-
-
-//        doublyLinked.DoubleLink theList = new doublyLinked.DoubleLink(); // Создание списка
-//        doublyLinked.DoubleLinkedListIterator iter1 = theList.getIterator(); // Создание итератора
-//        long value;
-//        iter1.insertAfter(20); // Вставка элементов
-//        iter1.insertAfter(40);
-//        iter1.insertAfter(80);
-//
-//        while(true)
-//        {
-//            System.out.print("Enter first letter of show, reset, ");
-//            System.out.print("next, get, before, after, delete: ");
-//            System.out.flush();
-//            char choice = getChar(); // Ввод команды
-//            switch(choice)
-//            {
-//                case 's': // Вывод списка
-//                    if( !theList.isEmpty() )
-//                        theList.displayForward();
-//                    else
-//                        System.out.println("List is empty");
-//                    break;
-//                case 'r': // Возврат к первому элементу
-//                    iter1.reset();
-//                    break;
-//                case '-':
-//                    if (!theList.isEmpty() && iter1.getCurrent().previous!=null)
-//                    iter1.previousLink();
-//                    else
-//                        System.out.println("Cant go back");
-//                    break;
-//                case 'n': // Переход к следующему элементу
-//                    if( !theList.isEmpty() && !iter1.atEnd() )
-//                        iter1.nextLink();
-//                    else
-//                        System.out.println("Can’t go to next link");
-//                    break;
-//                case 'g': // Получение текущего элемента
-//                    if( !theList.isEmpty() )
-//                    {
-//                        value = iter1.getCurrent().dData;
-//                        System.out.println("Returned " + value);
-//                    }
-//                    else
-//                        System.out.println("List is empty");
-//                    break;
-//                case 'b': // Вставка перед текущим элементом
-//                    System.out.print("Enter value to insert: ");
-//                    System.out.flush();
-//                    value = getInt();
-//                    iter1.insertBefore(value);
-//                    break;
-//                case 'a': // Вставка после текущего элемента
-//                    System.out.print("Enter value to insert: ");
-//                    System.out.flush();
-//                    value = getInt();
-//                    iter1.insertAfter(value);
-//                    break;
-//                case 'd': // Удаление текущего элемента
-//                    if( !theList.isEmpty() )
-//                    {
-//                        value = iter1.deleteCurrent();
-//                        System.out.println("Deleted " + value);
-//                    }
-//                    else
-//                        System.out.println("Can’t delete");
-//                    break;
-//                default:
-//                    System.out.println("Invalid entry");
-//            }
-//        }
-
+        Tree w1 = new Tree();
+        w1.insert(81);
+        w1.insert(72);
+        w1.insert(86);
+        w1.insert(82);
+        w1.insert(83);
+        w1.insert(89);
+        w1.insert(87);
+        w1.insert(88);
+        w1.displayTree();
+        w1.preOrder(w1.getroot());
+        System.out.println();
+        w1.inOrder(w1.getroot());
+        System.out.println();
+        w1.postOrder(w1.getroot());
 
     }
 
--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -1,8 +1,13 @@
 package Tree;
-
-class Tree
+import java.io.*;
+import java.util.*;
+public class Tree
 {
     private Node root; // Единственное поле данных
+    public Node getroot()
+    {
+        return root;
+    }
     public Node find(int key) // Поиск узла с заданным ключом
     { // (предполагается, что дерево не пустое)
         Node current = root; // Начать с корневого узла
@@ -17,7 +22,7 @@ class Tree
         }
         return current; // Элемент найден
     }
-    public void insert(int id, double dd)
+    public void insert(int id)
     {
         Node newNode = new Node(); // Создание нового узла
         newNode.iData = id; // Вставка данных
@@ -53,10 +58,133 @@ class Tree
         }
     }
     // -------------------------------------------------------------
-    public void delete(int id)
+    public boolean delete(int key)
     {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+        while(current.iData != key) // Поиск узла
+        {
+            parent = current;
+            if(key < current.iData) // Двигаться налево?
+            {
+                isLeftChild = true;
+                current = current.leftChild;
+            }
+            else // Или направо?
+            {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+            if(current == null) // Конец цепочки
+                return false; // Узел не найден
+        }
+        // Удаляемый узел найден
+        // Если узел не имеет потомков, он просто удаляется.
+        if(current.leftChild==null &&
+                current.rightChild==null)
+        {
+            if(current == root) // Если узел является корневым,
+                root = null; // дерево очищается
+            else if(isLeftChild)
+                parent.leftChild = null; // Узел отсоединяется
+            else // от родителя
+                parent.rightChild = null;
+        }
+        // Если нет правого потомка, узел заменяется левым поддеревом
+        else if(current.rightChild==null)
+            if(current == root)
+                root = current.leftChild;
+            else if(isLeftChild)
+                parent.leftChild = current.leftChild;
+            else
+                parent.rightChild = current.leftChild;
+            // Если нет левого потомка, узел заменяется правым поддеревом
+        else if(current.leftChild==null)
+            if(current == root)
+                root = current.rightChild;
+            else if(isLeftChild)
+                parent.leftChild = current.rightChild;
+            else
+                parent.rightChild = current.rightChild;
+        else // Два потомка, узел заменяется преемником
+        {
+            // Поиск преемника для удаляемого узла (current)
+            Node successor = getSuccessor(current);
+            // Родитель current связывается с посредником
+            if(current == root)
+                root = successor;
+            else if(isLeftChild)
+                parent.leftChild = successor;
+            else
+                parent.rightChild = successor;
+            // Преемник связывается с левым потомком current
+            successor.leftChild=current.leftChild;
+            return true;}
+        return false;
+        }
+
+
+    // Метод возвращает узел со следующим значением после delNode.
+    // Для этого он сначала переходит к правому потомку, а затем
+    // отслеживает цепочку левых потомков этого узла.
+    private Node getSuccessor(Node delNode)
+    {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.rightChild; // Переход к правому потомку
+        while(current != null) // Пока остаются левые потомки
+        {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild; // Переход к левому потомку
+        }
+        // Если преемник не является
+        if(successor != delNode.rightChild) // правым потомком,
+        { // создать связи между узлами
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+        return successor;
     }
-    private void inOrder(Node localRoot)
+    public void traverse(int traverseType)
+    {
+        switch(traverseType)
+        {
+            case 1: System.out.print("\nPreorder traversal: ");
+                preOrder(root);
+                break;
+            case 2: System.out.print("\nInorder traversal: ");
+                inOrder(root);
+                break;
+            case 3: System.out.print("\nPostorder traversal: ");
+                postOrder(root);
+                break;
+        }
+        System.out.println();
+    }
+
+
+    public void preOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            System.out.print(localRoot.iData + " ");
+            preOrder(localRoot.leftChild);
+            preOrder(localRoot.rightChild);
+        }
+    }
+    public void postOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            postOrder(localRoot.leftChild);
+            postOrder(localRoot.rightChild);
+            System.out.print(localRoot.iData + " ");
+        }
+    }
+
+    public void inOrder(Node localRoot)
     {
         if(localRoot != null)
         {
@@ -65,6 +193,53 @@ class Tree
             inOrder(localRoot.rightChild);
         }
     }
+
+    public void displayTree()
+    {
+        Stack globalStack = new Stack();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println(
+                "......................................................");
+        while(isRowEmpty==false)
+        {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+            for(int j=0; j<nBlanks; j++)
+                System.out.print(' ');
+            while(globalStack.isEmpty()==false)
+            {
+                Node temp = (Node)globalStack.pop();
+                if(temp != null)
+                {
+                    System.out.print(temp.iData);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+                    if(temp.leftChild != null ||
+                            temp.rightChild != null)
+                        isRowEmpty = false;
+                }
+                else
+                {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for(int j=0; j<nBlanks*2-2; j++)
+                    System.out.print(' ');
+            }
+            System.out.println();
+            nBlanks /= 2;
+            while(localStack.isEmpty()==false)
+                globalStack.push( localStack.pop() );
+        }
+        System.out.println(
+                "......................................................");
+    }
+// -------------------------------------------------------------
+
+
     public Node minimum() // Возвращает узел с минимальным ключом
     {
         Node current;
